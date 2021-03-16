f20d1d8739cc68861392f229ee2103dff0c08b45
stephentreacy <stephen.treacy92@gmail.com>
2021-03-16 22:58:17 +0000

    Replies with a message per item

--- warfarm_bot.py
+++ warfarm_bot.py
@@ -12,14 +12,20 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
+    #Ignores messages sent by bot
     if message.author == client.user:
         return
 
+    #Test case to check bot is replying
     if message.content.startswith('$hi'):
         await message.channel.send('hello')
 
+    #Takes link and replies with messages containing item orders
     if message.content.startswith('$link'):
+        #Delete the message to prevent others using the link
         await message.delete()
+
         try:
             link = message.content.split()[1]
         except:
@@ -35,12 +41,22 @@ async def on_message(message):
                     df_orders = pd.json_normalize(orders['orders'])
                     df_sell = df_orders[(df_orders['user.status'] == 'ingame') & (df_orders['order_type'] == 'sell')]
                     df_buy = df_orders[(df_orders['user.status'] == 'ingame') & (df_orders['order_type'] == 'buy')]
-                    sell_orders = df_sell.nsmallest(5, 'platinum')['platinum'].values
-                    buy_orders = df_buy.nlargest(5, 'platinum')['platinum'].values
+                    sell_orders = df_sell.nsmallest(5, 'platinum')['platinum'].values.tolist()
+                    sell_orders = ', '.join(str(int(order)) for order in sell_orders) if sell_orders else 'None'
+                    buy_orders = df_buy.nlargest(5, 'platinum')['platinum'].values.tolist()
+                    buy_orders = ', '.join(str(int(order)) for order in buy_orders) if buy_orders else 'None'
                     item_orders.update({item : [sell_orders, buy_orders, item.lower().replace(' ','_').replace('-','_').replace("'",'').replace('&','and')]})
+
+                    #Create Embeded message with item information
+                    embed_item = discord.Embed(title=item)
+                    embed_item.add_field(name="Buy Orders", value=buy_orders, inline=False)
+                    embed_item.add_field(name="Sell Orders", value=sell_orders, inline=False)
+                    await message.channel.send(embed=embed_item)
 
                 time.sleep(0.4)
 
-            await message.channel.send(item_orders)
+            #await message.channel.send(item_orders)
+
+
 
 client.run(os.getenv('TOKEN'))
